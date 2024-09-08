b88c9689ad00d495e8024540ef76429af2a4c7b2
smharis-dev <dev.mohammad.haris@gmail.com>
2024-09-08 03:45:01 -0700

    updated api routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,6 @@ import userRoutes from "./app/user/routes";
 import authRoutes from "./app/auth/routes";
 
 export const routes = {
-  "/user": userRoutes,
-  "/auth": authRoutes,
+  "/api/user": userRoutes,
+  "/api/auth": authRoutes,
 };
